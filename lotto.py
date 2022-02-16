e2ff5b9868ee9fae38e032bc04b09a024109520e
whit33y <troppout@gmail.com>
2022-02-16 20:37:49 +0100

    Minor visual fixes added

--- lotto.py
+++ lotto.py
@@ -2,17 +2,22 @@ from itertools import count
 import random
 import math
 
+
 picked_numbers = []
 all_numbers = []
 random_numbers = []
 is_six = False
 count_numbers=0
+hit = 0
 
+
+#draw and add  6 random numbers to list between 1 and 49 without repetition
 for a in range(1,50):
     all_numbers.append(a)
 random_numbers = random.sample(all_numbers, 6)
-print(random_numbers)
 
+
+#asking user for 6 numbers between 1 and 49 without repetition
 while is_six==False:
     number = input("Pass number from 1 to 49: ")
     result = number.isdigit()
@@ -23,11 +28,18 @@ while is_six==False:
             picked_numbers = list(set(picked_numbers))
     if len(picked_numbers) == 6:
         is_six = True
-hit = 0
+
+
+#check how many numbers fits
 for x in range(6):
     for y in range(6):
         if picked_numbers[x] == random_numbers[y]:
             hit+=1
+
+    
+#print return message for user 
+for x in range(len(random_numbers)):
+    print(x+1 ,".drawed number: ", random_numbers[x])
 if(hit==0):
     print('Nothing! ;(')
 elif(hit==1):
